042ae8a6b9618bcd7faba143469ebf9913c37a19
Emad Anwer <emadanwer.official@gmail.com>
2023-06-22 20:43:41 +0200

    test

--- rotl_rotr.c
+++ rotl_rotr.c
@@ -1,31 +1,35 @@
 #include "monty.h"
 
-
 /*
 
 
- 1 2 3 4 5 6 7 8 9 0
+   1 2 3 4 5 6 7 8 9
  0 1 2 3 4 5 6 7 8 9
 rotl
 
 */
 void rotl(stack_t **stack, unsigned int line_number)
 {
+	int top;
+	stack_t *c;
 	UNUSED(line_number);
-	if(stack != NULL || *stack != NULL || (*stack)->next != NULL)
+	if (stack != NULL || *stack != NULL || (*stack)->next != NULL)
 	{
-		add_stackint_end(stack , (*stack)->n);
-		delete_top_node(stack);
+		top = (*stack)->n;
+		c = *stack;
+		while (c->next)
+		{
+			c->n = c->next->n;
+			c = c->next;
+		}
+		c->n = top;
 	}
-
 }
-
 
 void rotr(stack_t **stack, unsigned int line_number)
 {
-	if(stack == NULL || *stack == NULL)
+	if (stack == NULL || *stack == NULL)
 		print_pint_error(line_number);
 
 	printf("%d\n", (*stack)->n);
-
 }
