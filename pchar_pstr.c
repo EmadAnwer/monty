a528416588bbf55cfa78e0a4cf962475c2f2c5a1
Magedsaif <magedsaif21@gmail.com>
2023-06-22 18:25:45 +0200

    pchar

--- pchar_pstr.c
+++ pchar_pstr.c
@@ -2,6 +2,27 @@
 
 
 
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	int num;
+
+	if(stack == NULL || *stack == NULL)
+		print_pchar_error(line_number);
+
+	num = (*stack)->n ;
+	if ((*stack)->n)
+	{
+		if (num >= 0 && num <= 127) 
+		{
+        char ch = (char)num;
+		printf("%c\n", ch);
+		}
+		else
+			print_pchar_error(line_number);
+	}
+}
+
+
 void pstr(stack_t **stack, unsigned int line_number)
 {
 	if(stack == NULL || *stack == NULL)
@@ -11,12 +32,9 @@ void pstr(stack_t **stack, unsigned int line_number)
 
 }
 
-
-void pchar(stack_t **stack, unsigned int line_number)
+void print_pchar_error(int line_number)
 {
-	if(stack == NULL || *stack == NULL)
-		print_pint_error(line_number);
-
-	printf("%d\n", (*stack)->n);
-
+	fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+	free_everything();
+	exit(EXIT_FAILURE);
 }
