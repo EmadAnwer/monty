e37103b158baa64644409b33f93f7c00536924c8
Magedsaif <magedsaif21@gmail.com>
2023-06-22 17:37:23 +0200

    edit pop

--- linkedlist_functions.c
+++ linkedlist_functions.c
@@ -101,11 +101,6 @@ void free_stack(stack_t *head)
 void delete_top_node(stack_t **stack)
 {
 	stack_t *temp;
-	if (*stack == NULL)
-	{
-		printf("Error: Stack is empty.\n");
-		return;
-	}
 
 	temp = *stack;
 	*stack = (*stack)->next;
