5841b4048098e58a1ef41f5cb9e63e89f29c97bc
Emad Anwer <emadanwer.official@gmail.com>
2023-06-22 16:06:49 +0200

    swap

--- pint_swap.c
+++ pint_swap.c
@@ -1,6 +1,5 @@
 #include "monty.h"
 
-
 /*
 
 	prints the value at the top of the stack, followed by a new line.
@@ -11,22 +10,19 @@
 */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if(stack == NULL || *stack == NULL)
+	if (stack == NULL || *stack == NULL)
 		print_pint_error(line_number);
 
 	printf("%d\n", (*stack)->n);
-
 }
-
 
 void print_pint_error(int line_number)
 {
-	
+
 	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 	free_everything();
 	exit(EXIT_FAILURE);
 }
-
 
 /*
 
@@ -41,7 +37,10 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	int tmp;
 
-	if((*stack)->next)
+	if (stack == NULL || *stack == NULL)
+		print_swap_error(line_number);
+		
+	if ((*stack)->next)
 	{
 		tmp = (*stack)->n;
 		(*stack)->n = (*stack)->next->n;
@@ -51,10 +50,9 @@ void swap(stack_t **stack, unsigned int line_number)
 		print_swap_error(line_number);
 }
 
-
 void print_swap_error(int line_number)
 {
-	
+
 	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 	free_everything();
 	exit(EXIT_FAILURE);
